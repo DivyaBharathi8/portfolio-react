efe9e62baabcce52d8beb89dcc90eae0d334b6f8
DivyaBharathi8 <divyabharathi0807@gmail.com>
2025-09-10 10:21:52 +0530

    changes

--- src/components/cards/cards-container.js
+++ src/components/cards/cards-container.js
@@ -1,4 +1,5 @@
-import Cards from "./cards";
+import { Cards, Cards2, Cards3, Cards4 } from "./cards";
+
 import "./style.css";
 
 export function CardsContainer() {
@@ -9,24 +10,17 @@ export function CardsContainer() {
       description:
         "Crafting unique, responsive websites using HTML, CSS, JavaScript & Bootstrap — tailored for performance, design precision, and brand identity. Crafting unique, responsive websites using HTML, CSS, JavaScript",
     },
+  ];
+
+  const cardsContent2 = [
     {
       number: "02",
       title: "Custom Website Development",
       description:
         "Designing and developing dynamic, responsive websites using custom themes, plugins, and optimized layouts in WordPress.",
     },
-    {
-      number: "03",
-      title: "Custom Website Development",
-      description:
-        "Designing and developing dynamic, responsive websites using custom themes, plugins, and optimized layouts in WordPress.",
-    },
-    {
-      number: "04",
-      title: "Custom Website Development",
-      description:
-        "Crafting unique, responsive websites using HTML, CSS, JavaScript & Bootstrap — tailored for performance, design precision, and brand identity. Crafting unique, responsive websites using HTML, CSS, JavaScript",
-    },
+  ];
+  const cardsContent3 = [
     {
       number: "04",
       title: "Custom Website Development",
@@ -35,16 +29,62 @@ export function CardsContainer() {
     },
   ];
 
+  const cardsContent4 = [
+    {
+      number: "03",
+      title: "Custom Website Development",
+      description:
+        "Designing and developing dynamic, responsive websites using custom themes, plugins, and optimized layouts in WordPress.",
+    },
+  ];
+
   return (
-    <div className="card-section">
-      {cardsContent.map((card, idx) => (
-        <Cards
-          key={idx}
-          number={card.number}
-          title={card.title}
-          description={card.description}
-        />
-      ))}
+    <div>
+      <div className="card-section-top">
+        <div className="card-left">
+          {cardsContent.map((card, idx) => (
+            <Cards
+              key={idx}
+              number={card.number}
+              title={card.title}
+              description={card.description}
+            />
+          ))}
+        </div>
+        <div className="card-right">
+          {cardsContent2.map((values2, index) => (
+            <Cards2
+              keys={index}
+              number={values2.number}
+              title={values2.title}
+              description={values2.description}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="card-section-bottom">
+        <div className="card-right">
+          {cardsContent4.map((values4, indexes) => (
+            <Cards4
+              keys={indexes}
+              number={values4.number}
+              title={values4.title}
+              description={values4.description}
+            />
+          ))}
+        </div>
+        <div className="card-right">
+          {cardsContent3.map((values3, indexs) => (
+            <Cards3
+              keys={indexs}
+              number={values3.number}
+              title={values3.title}
+              description={values3.description}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,6 +1,4 @@
-import React from "react";
-
-export default function Cards(props) {
+const Cards = (props) => {
   const { number, title, description } = props;
   return (
     <div className="card-container">
@@ -16,4 +14,60 @@ export default function Cards(props) {
       <p className="card-desc">{description}</p>
     </div>
   );
-}
+};
+
+const Cards2 = (props) => {
+  const { number, title, description } = props;
+  return (
+    <div className="card-container">
+      <div>
+        <div className="card-number">{number}</div>
+        <div className="icon-bg">
+          <p>
+            <i className="fas fa-arrow-right card-arrow"></i>
+          </p>
+        </div>
+      </div>
+      <h3 className="card-title">{title}</h3>
+      <p className="card-desc">{description}</p>
+    </div>
+  );
+};
+
+const Cards3 = (props) => {
+  const { number, title, description } = props;
+  return (
+    <div className="card-container">
+      <div>
+        <div className="card-number">{number}</div>
+        <div className="icon-bg">
+          <p>
+            <i className="fas fa-arrow-right card-arrow"></i>
+          </p>
+        </div>
+      </div>
+      <h3 className="card-title">{title}</h3>
+      <p className="card-desc">{description}</p>
+    </div>
+  );
+};
+
+const Cards4 = (props) => {
+  const { number, title, description } = props;
+  return (
+    <div className="card-container">
+      <div>
+        <div className="card-number">{number}</div>
+        <div className="icon-bg">
+          <p>
+            <i className="fas fa-arrow-right card-arrow"></i>
+          </p>
+        </div>
+      </div>
+      <h3 className="card-title">{title}</h3>
+      <p className="card-desc">{description}</p>
+    </div>
+  );
+};
+
+export { Cards, Cards2, Cards3, Cards4 };
